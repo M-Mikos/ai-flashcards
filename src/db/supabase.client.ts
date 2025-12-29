303985e94f862a25bcef5639b62cc655157f83be
M-Mikos <mikos.marcin.m@gmail.com>
2025-12-29 15:27:44 +0100

    chore: fix line ending linting

--- src/db/supabase.client.ts
+++ src/db/supabase.client.ts
@@ -1,14 +1,13 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
 
-import type { Database } from './database.types.ts';
+import type { Database } from "./database.types.ts";
 
 const supabaseUrl = import.meta.env.SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.SUPABASE_KEY;
 
 if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error('Missing SUPABASE_URL or SUPABASE_KEY environment variable');
+  throw new Error("Missing SUPABASE_URL or SUPABASE_KEY environment variable");
 }
 
 export const supabaseClient = createClient<Database>(supabaseUrl, supabaseAnonKey);
 export type SupabaseClient = typeof supabaseClient;
-
