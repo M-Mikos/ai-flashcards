194d26fb4fbb18206bdf0f55e6e870e2812b7c40
M-Mikos <mikos.marcin.m@gmail.com>
2025-12-29 15:22:50 +0100

    feat: init supabase in astro, add db types

---
+++ src/db/supabase.client.ts
@@ -0,0 +1,14 @@
+import { createClient } from '@supabase/supabase-js';
+
+import type { Database } from './database.types.ts';
+
+const supabaseUrl = import.meta.env.SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.SUPABASE_KEY;
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error('Missing SUPABASE_URL or SUPABASE_KEY environment variable');
+}
+
+export const supabaseClient = createClient<Database>(supabaseUrl, supabaseAnonKey);
+export type SupabaseClient = typeof supabaseClient;
+
